942cd96c60a76e24a03c6036089bc3177509ac91
Arthur Lang <aargolang@gmail.com>
2022-04-24 15:09:22 -0700

    added ini file into config

--- mk.bat
+++ mk.bat
@@ -3,5 +3,6 @@ REM "mk a" to make all and install
 IF "%1"=="a" (
     copy Textures\InstaFlakTextures.utx ..\Textures
     copy Animations\InstaFlak.ukx ..\Animations
+    copy System\InstaFlakConfig.ini ..\System
 )
 del ..\System\InstaFlak.* && ..\System\ucc.exe make
