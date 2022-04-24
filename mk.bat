eb882bd5d2ebab6710fef8435e30e6ce309e98a8
Arthur Lang <aargolang@gmail.com>
2022-04-24 14:04:34 -0700

    added textures for InstaFlak, added texture installation parameter to mk file

--- mk.bat
+++ mk.bat
@@ -1,1 +1,7 @@
+@echo OFF
+REM "mk a" to make all and install
+IF "%1"=="a" (
+    copy Textures\InstaFlakTextures.utx ..\Textures
+    copy Animations\InstaFlak.ukx ..\Animations
+)
 del ..\System\InstaFlak.* && ..\System\ucc.exe make
